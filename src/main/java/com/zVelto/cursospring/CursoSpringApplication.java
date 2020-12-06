4d5ecf163896d1e10c32f3dcbd7c9993bb96e877
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-05 22:27:11 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/zVelto/cursospring/CursoSpringApplication.java
+++ src/main/java/com/zVelto/cursospring/CursoSpringApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.zVelto.cursospring.domain.Categoria;
+import com.zVelto.cursospring.domain.Produto;
 import com.zVelto.cursospring.domain.repositories.CategoriaRepository;
+import com.zVelto.cursospring.domain.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository catRepo;
+	@Autowired
+	private ProdutoRepository pRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -26,7 +30,19 @@ public class CursoSpringApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		catRepo.saveAll(Arrays.asList(cat1, cat2));
+		pRepo.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
