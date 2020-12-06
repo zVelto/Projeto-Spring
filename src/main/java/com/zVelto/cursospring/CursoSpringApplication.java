09391dfb9811942f67ad4ed946a8ad56be361ba4
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-05 21:51:02 -0300

    Criando operação de instanciação

--- src/main/java/com/zVelto/cursospring/CursoSpringApplication.java
+++ src/main/java/com/zVelto/cursospring/CursoSpringApplication.java
@@ -1,13 +1,32 @@
 package com.zVelto.cursospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.zVelto.cursospring.domain.Categoria;
+import com.zVelto.cursospring.domain.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursoSpringApplication {
+public class CursoSpringApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository catRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		catRepo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
