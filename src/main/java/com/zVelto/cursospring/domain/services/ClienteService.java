2df7331bbdf72a2b5bdf9b8bc3fbff31251609f4
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-08 20:20:44 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/com/zVelto/cursospring/domain/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.zVelto.cursospring.domain.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zVelto.cursospring.domain.Cliente;
+import com.zVelto.cursospring.domain.repositories.ClienteRepository;
+import com.zVelto.cursospring.domain.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", tipo:" + Cliente.class.getName());
+		}
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", tipo:" + Cliente.class.getName()));
+	}
+}
