472d4ffd0c101bb660252d25d1546acb373fb264
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-09 20:35:44 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/zVelto/cursospring/domain/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.zVelto.cursospring.domain.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zVelto.cursospring.domain.Pedido;
+import com.zVelto.cursospring.domain.repositories.PedidoRepository;
+import com.zVelto.cursospring.domain.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", tipo:" + Pedido.class.getName());
+		}
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", tipo:" + Pedido.class.getName()));
+	}
+}
