14b3b80ab3e690434d0649ad95f7904223a9650f
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-12 23:47:06 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/zVelto/cursospring/domain/services/ClienteService.java
+++ src/main/java/com/zVelto/cursospring/domain/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.zVelto.cursospring.domain.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,17 +12,25 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.zVelto.cursospring.domain.Cidade;
 import com.zVelto.cursospring.domain.Cliente;
+import com.zVelto.cursospring.domain.Endereco;
+import com.zVelto.cursospring.domain.enums.TipoCliente;
 import com.zVelto.cursospring.domain.repositories.ClienteRepository;
+import com.zVelto.cursospring.domain.repositories.EnderecoRepository;
 import com.zVelto.cursospring.domain.services.exceptions.DataIntegrityException;
 import com.zVelto.cursospring.domain.services.exceptions.ObjectNotFoundException;
 import com.zVelto.cursospring.dto.ClienteDTO;
+import com.zVelto.cursospring.dto.ClienteNewDTO;
 
 @Service
 public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository endRepo;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -30,9 +40,12 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", tipo:" + Cliente.class.getName()));
 	}
 	
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return repo.save(obj);
+		obj = repo.save(obj);
+		endRepo.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -62,6 +75,21 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeID(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if(objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if(objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
--- src/main/java/com/zVelto/cursospring/resources/ClienteResource.java
+++ src/main/java/com/zVelto/cursospring/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.zVelto.cursospring.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,10 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.zVelto.cursospring.domain.Cliente;
 import com.zVelto.cursospring.domain.services.ClienteService;
 import com.zVelto.cursospring.dto.ClienteDTO;
+import com.zVelto.cursospring.dto.ClienteNewDTO;
 
 @RestController
 @RequestMapping(value="/clientes")
@@ -31,6 +34,15 @@ public class ClienteResource {
 		
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
