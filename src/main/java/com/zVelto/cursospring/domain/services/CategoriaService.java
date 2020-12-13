b8f475a98f0659f5ba65a758fbeb0e6df02cfdcc
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-12 22:42:07 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/zVelto/cursospring/domain/services/CategoriaService.java
+++ src/main/java/com/zVelto/cursospring/domain/services/CategoriaService.java
@@ -36,8 +36,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -62,4 +63,9 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
+	
 }
