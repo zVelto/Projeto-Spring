a7c4e7963158333c11bcf9e8852cbaa5f9d5fd2e
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-13 15:17:07 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/zVelto/cursospring/dto/ClienteDTO.java
+++ src/main/java/com/zVelto/cursospring/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.zVelto.cursospring.domain.Cliente;
+import com.zVelto.cursospring.domain.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
