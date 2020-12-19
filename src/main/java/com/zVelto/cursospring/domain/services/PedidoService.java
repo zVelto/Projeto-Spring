31ebef7c5f102b4fa8d8618fc67bbdafc97e53e6
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-19 20:38:30 -0300

    Email HTML

--- src/main/java/com/zVelto/cursospring/domain/services/PedidoService.java
+++ src/main/java/com/zVelto/cursospring/domain/services/PedidoService.java
@@ -68,7 +68,7 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		iPRepo.saveAll(obj.getItens());
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 }
