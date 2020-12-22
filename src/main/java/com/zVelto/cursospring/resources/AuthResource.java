c49990c78adb2614cf4fc89da4f25417ef55036f
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-22 17:11:45 -0300

    Adicionando opção 'Esqueci a senha'

--- src/main/java/com/zVelto/cursospring/resources/AuthResource.java
+++ src/main/java/com/zVelto/cursospring/resources/AuthResource.java
@@ -1,14 +1,18 @@
 package com.zVelto.cursospring.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zVelto.cursospring.domain.services.AuthService;
 import com.zVelto.cursospring.domain.services.UserService;
+import com.zVelto.cursospring.dto.EmailDTO;
 import com.zVelto.cursospring.security.JWTUtil;
 import com.zVelto.cursospring.security.UserSS;
 
@@ -18,12 +22,21 @@ public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
