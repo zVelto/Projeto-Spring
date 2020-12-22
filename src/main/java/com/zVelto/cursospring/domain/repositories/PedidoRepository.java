f12918f8ae043f1067e1153b17d896aa465bacfe
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-22 16:40:30 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/zVelto/cursospring/domain/repositories/PedidoRepository.java
+++ src/main/java/com/zVelto/cursospring/domain/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package com.zVelto.cursospring.domain.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.zVelto.cursospring.domain.Cliente;
 import com.zVelto.cursospring.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- src/main/java/com/zVelto/cursospring/domain/services/PedidoService.java
+++ src/main/java/com/zVelto/cursospring/domain/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.zVelto.cursospring.domain.Cliente;
 import com.zVelto.cursospring.domain.ItemPedido;
 import com.zVelto.cursospring.domain.PagamentoComBoleto;
 import com.zVelto.cursospring.domain.Pedido;
@@ -14,7 +18,9 @@ import com.zVelto.cursospring.domain.enums.EstadoPagamento;
 import com.zVelto.cursospring.domain.repositories.ItemPedidoRepository;
 import com.zVelto.cursospring.domain.repositories.PagamentoRepository;
 import com.zVelto.cursospring.domain.repositories.PedidoRepository;
+import com.zVelto.cursospring.domain.services.exceptions.AuthorizationException;
 import com.zVelto.cursospring.domain.services.exceptions.ObjectNotFoundException;
+import com.zVelto.cursospring.security.UserSS;
 
 @Service
 public class PedidoService {
@@ -70,5 +76,15 @@ public class PedidoService {
 		iPRepo.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
