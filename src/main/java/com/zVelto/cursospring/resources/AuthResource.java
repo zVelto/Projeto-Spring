6b65a2c868ff178c31fbdb59c31e4db6a2f4181b
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-22 16:54:04 -0300

    Refresh token

---
+++ src/main/java/com/zVelto/cursospring/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.zVelto.cursospring.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zVelto.cursospring.domain.services.UserService;
+import com.zVelto.cursospring.security.JWTUtil;
+import com.zVelto.cursospring.security.UserSS;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
