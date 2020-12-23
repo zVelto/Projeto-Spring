295c149685c033f863d819246154f8d528c07c0b
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-23 16:55:40 -0300

    Enviando imagem via endpoint

--- src/main/java/com/zVelto/cursospring/CursoSpringApplication.java
+++ src/main/java/com/zVelto/cursospring/CursoSpringApplication.java
@@ -1,25 +1,18 @@
 package com.zVelto.cursospring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.zVelto.cursospring.domain.services.S3Service;
-
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner{
-	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\welton\\Pictures\\op.jpg");	
+	public void run(String... args) throws Exception {	
 	}
 
 }
--- src/main/java/com/zVelto/cursospring/domain/services/ClienteService.java
+++ src/main/java/com/zVelto/cursospring/domain/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.zVelto.cursospring.domain.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.zVelto.cursospring.domain.Cidade;
 import com.zVelto.cursospring.domain.Cliente;
@@ -38,6 +40,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository endRepo;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		
@@ -108,6 +113,10 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 	
 }
