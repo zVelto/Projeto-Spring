0b2fe70c008a70faf0591091f04cc3b92500950a
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-12-23 16:18:39 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/zVelto/cursospring/CursoSpringApplication.java
+++ src/main/java/com/zVelto/cursospring/CursoSpringApplication.java
@@ -1,12 +1,17 @@
 package com.zVelto.cursospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.zVelto.cursospring.domain.services.S3Service;
+
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner{
 	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -14,9 +19,7 @@ public class CursoSpringApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
-		
+		s3Service.uploadFile("C:\\Users\\welton\\Pictures\\op.jpg");	
 	}
 
 }
